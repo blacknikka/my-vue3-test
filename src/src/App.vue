021e7ea3a5af01f07f64684d0994782505b93b76
Yasuhiro <yasuhiro1026a@hotmail.com>
2021-10-05 01:34:56 +0900

    Applicate type script

--- src/src/App.vue
+++ src/src/App.vue
@@ -1,19 +1,9 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <ScoreLists></ScoreLists>>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+  </div>
+  <router-view/>
 </template>
-
-<script>
-// import HelloWorld from './components/HelloWorld.vue'
-import ScoreLists from './components/Score/ScoreLists.vue'
-
-export default {
-  name: 'App',
-  components: {
-    ScoreLists
-  }
-}
-</script>
 
 <style>
 #app {
@@ -22,6 +12,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+  <img alt="Vue logo" src="@/assets/logo.png">
+  <ScoreLists></ScoreLists>>
+  
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ScoreLists from '@/views/Score/ScoreLists.vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    ScoreLists,
+  },
+});
+</script>
